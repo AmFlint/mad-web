5e2af6cd3595b789ebe17c8ce8c6c33b9a159c5d
Antoine Masselot <amasselot@online.net>
2020-04-13 19:26:36 +0200

    Add Title

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1>Client Monitoring</h1>
         {
           this.state.clients.map(client => (
             <div key={client.socketID}>
